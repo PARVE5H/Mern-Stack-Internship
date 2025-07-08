356a898f9eec264b78c0bb3b6b1a9caed110cd11
Parvesh Bansal <parveshbansal063@gmail.com>
2025-07-08 18:29:45 +0530

    Add deploy dependencies

--- task1/vite.config.js
+++ task1/vite.config.js
@@ -1,12 +1,11 @@
-/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Mern-Stack-Internship/task1",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
